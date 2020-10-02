9bc92083fb7dc93b5a83ad80939bc2c4e69302a4
Joaouriel10 <joaouriel2016@outlook.com>
2020-10-01 22:50:54 -0300

    Criação middleware JWT

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,7 +1,14 @@
 export const existingUser = {
+  status: "false",
   message: 'there is already a registration with this email'
 };
 
 export const invalidCredentials = {
+  status: "false",
   message: 'Incorrect email/password combination.'
+}
+
+export const invalidJWT = {
+  status: "false",
+  message: 'Invalid JTW Token'
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,10 +3,13 @@ import { Router } from 'express';
 import usersRouter from './user.routes';
 import sessionsRouter from './session.routes';
 
+import middlewareJwt from '../middleware/middlewareJWT';
+
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/session', sessionsRouter);
+routes.use(middlewareJwt);
 
 routes.get('/', (request, response) => {
   return response.status(200).json({ ok: 'true' });
