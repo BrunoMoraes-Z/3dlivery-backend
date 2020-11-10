9e46e71ac563afb1dd97a40e93c493b63cc438a7
Bruno Moraes <moraes.7bruno@gmail.com>
2020-11-10 00:13:57 -0300

    adicionado validador do tipo de arquivo enviado.

--- src/routes/drawing.routes.ts
+++ src/routes/drawing.routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 
 import config from '../config/upload';
 import middlewareJwt from '../middleware/middlewareJWT';
+import AppError from '../errors/AppError';
 
 const upload = multer(config);
 const router = Router();
@@ -11,55 +12,16 @@ router.use(middlewareJwt);
 
 router.post('/', upload.single('drawing'), async (request, response) => {
 
+  const file = request.file;
+  const type = file.originalname.split(".")[file.originalname.split(".").length - 1]
+
+  if (type !== 'stl') {
+    throw new AppError('Formato do arquivo incorreto.', 400);
+  }
+
   console.log(request.file);
 
   return response.json({ status: 'OK' });
 });
 
 export default router;
-
-// import CreateUserService from '../services/user/CreateUserService';
-// import AlterUserService from '../services/user/AlterUserSerice';
-// import middlewareJwt from '../middleware/middlewareJWT';
-// import AppError from '../errors/AppError';
-
-// const usersRouter = Router();
-
-// usersRouter.post('/', async (request, response) => {
-//   const { name, email, password } = request.body;
-
-//   const createUser = new CreateUserService();
-
-//   const user = await createUser.execute({ name, email, password });
-
-//   if (!user) {
-//     throw new AppError('Erro ao criar usuario.', 400);
-//   }
-
-//   return response.status(200).json({status: "success", user});
-// });
-
-// usersRouter.use(middlewareJwt);
-
-
-// usersRouter.patch('/', async (request, response) => {
-//   const { name, email, password } = request.body;
-
-//   if (!name && !email && !password) {
-//     throw new AppError('Favor enviar algum campo para atualização', 400);
-//   }
-
-//   const id = request.id;
-
-//   const AlterUserSerice = new AlterUserService();
-
-//   const updatedUser = await AlterUserSerice.execute({ id, name, email, password });
-
-//   if (!updatedUser) {
-//     throw new AppError('Erro ao cadastrar usuario.', 400);
-//   }
-
-//   return response.status(200).json({status: "success", updatedUser});
-// });
-
-// export default usersRouter;
