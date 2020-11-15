a45e1afb1bae6fc3f7d2b02d949bc2ceff576437
Bruno Moraes <moraes.7bruno@gmail.com>
2020-11-15 02:11:42 -0300

    corrigido problema de salvar arquivo fora do formato permitido.

--- src/routes/drawing.routes.ts
+++ src/routes/drawing.routes.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
 import multer from 'multer';
+import * as fs from 'fs';
+import path from 'path';
+// import * as stl from 'node-stl';
 
 import config from '../config/upload';
 import middlewareJwt from '../middleware/middlewareJWT';
@@ -10,17 +13,30 @@ import AppError from '../errors/AppError';
 
 const upload = multer(config);
 const router = Router();
+const tmpFolder = path.resolve(__dirname, '..', '..', 'temp');
 
 router.use(middlewareJwt);
 
 router.post('/', upload.single('drawing'), async (request, response) => {
 
   const file = request.file;
+  const name = file.filename;
   const type = file.originalname.split(".")[file.originalname.split(".").length - 1]
 
   if (type !== 'stl') {
+    fs.unlink(tmpFolder + '/' + name, (c) => {
+      if (c !== null) {
+        console.log(c);
+      }
+    });
     throw new AppError('Formato do arquivo incorreto.', 400);
   }
+
+  // try {
+  //   const s = stl(tmpFolder + "/" + name,);
+  // } catch (error) {
+  //   console.log(error);
+  // }
 
   return response.json({ status: 'OK' });
 });
